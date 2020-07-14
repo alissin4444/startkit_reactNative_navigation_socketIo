2550d62b4634356c825161dcbe3117b088748547
alissin4444 <dev.alissonsantos@gmail.com>
2020-07-14 19:50:06 -0300

    finalized

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,31 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+
+import { Text } from "react-native";
+
+import io from "socket.io-client";
 
 import { Container } from "./styles";
 
+const socketio = io("http://192.168.0.5:3333");
+
 const Home = ({ navigation }) => {
+  const [fruits, setFruits] = useState([]);
+
+  useEffect(() => {
+    socketio.on("@fruitApi/new_fruit", ({ fruit }) => {
+      setFruits((oldFruits) => [
+        ...oldFruits,
+        `${fruit} ${new Date().getTime()}`,
+      ]);
+    });
+  }, []);
+
   return (
-    <View justifyContent="center" alignItems="center" flex={1}>
-      <TouchableOpacity onPress={() => navigation.push("Fruits")}>
-        <Text>Visualize your fruits</Text>
-      </TouchableOpacity>
-    </View>
+    <Container>
+      {fruits.map((fruit) => (
+        <Text key={fruit}>{fruit}</Text>
+      ))}
+    </Container>
   );
 };
 
